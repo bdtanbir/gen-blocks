ee8d0f5474a41e9e41969567150d88f467c2fc15
Tanbir <tanbirsylhet20@gmail.com>
2026-02-02 19:59:28 +0600

    initial commit

---
+++ get-blocks.php
@@ -0,0 +1,14 @@
+<?php
+defined('ABSPATH') or die;
+
+/*
+Plugin Name: GenBlocks
+Description: Generate beautiful Gutenberg blocks instantly using AI. Turn simple text prompts into fully customizable WordPress blocks.
+Version: 1.0.0
+Author: bdtanbir
+Author URI: https://github.com/bdtanbir
+Plugin URI: https://github.com/bdtanbir/genblocks
+License: GPLv2 or later
+Text Domain: get-blocks
+Domain Path: /language
+*/
